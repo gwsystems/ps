85a65bbeda9cf6bcd5d3d9811907edcc6e73d222
ryuxin <m.phelps.a@gmail.com>
2018-01-02 19:05:47 -0500

    fix spelling issue

--- quiesce_type/temporal/ps_quiesce.c
+++ quiesce_type/temporal/ps_quiesce.c
@@ -4,5 +4,5 @@ void
 ps_quiesce(struct parsec *parsec, coreid_t curr, ps_tsc_t *qsc)
 {
 	(void)parsec, (void)curr;
-	*qsc = ps_tsc() - (ps_tsc_t)MAX_PARSEC_REPONSE;
+	*qsc = ps_tsc() - (ps_tsc_t)MAX_PARSEC_RESPONSE;
 }
