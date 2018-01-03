74a632b2c8fa4fcd0ec8dce6e589223f64575b55
ryuxin <m.phelps.a@gmail.com>
2018-01-02 19:06:17 -0500

    fix spacing issue

--- quiesce_type/real_time/ps_quiesce_rt.h
+++ quiesce_type/real_time/ps_quiesce_rt.h
@@ -2,7 +2,7 @@
 #define PS_QUIESCE_RT_H
 
 struct ps_quiescence_timing {
-	volatile ps_tsc_t     time_in, time_out;
+	volatile ps_tsc_t time_in, time_out;
 	char __padding[PS_CACHE_PAD_SZ(2*sizeof(ps_tsc_t))];
 } PS_ALIGNED PS_PACKED;
 
@@ -12,7 +12,7 @@ struct ps_smr_percore {
 } PS_ALIGNED PS_PACKED;
 
 struct ps_qsc_account {
-	size_t             qmemcnt;    /* # of items in the qsc_list */
+	size_t qmemcnt;    /* # of items in the qsc_list */
 };
 
 #endif	/* PS_QUIESCE_RT_H */
